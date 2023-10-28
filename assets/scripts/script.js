7a7e22a339420114c1a7b64bd0c9c42cf6e828ce
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-10-28 06:49:53 -0300

    Finished this project In this project I made on my own. I didn't watch the video to do this. Only searchs and some ChatGPT assistence to know the reason of my erros.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,6 +2,13 @@ const textareaEl = document.querySelector('#textarea');
 const totalEl = document.querySelector('.total');
 const remaining = document.querySelector('.remaining');
 
-remaining.innerHTML = document.querySelector('#textarea').maxLength
-totalEl.innerHTML = document.querySelector('#textarea').value.length;
+textareaEl.addEventListener("input", () => {
+  let text = textareaEl.value.length;
+  let remainingCharacter = textareaEl.maxLength - textareaEl.value.length;
+  totalEl.innerHTML = text;
+  remaining.innerHTML = remainingCharacter;
 
+  // if(remainingCharacter <= (remainingCharacter * 0.1)) {
+  //   remaining.style.setProperty("----remaining-color", "red");
+  // }
+})
